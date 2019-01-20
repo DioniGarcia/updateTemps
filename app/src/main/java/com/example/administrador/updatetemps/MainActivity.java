32e8d4a4189433c242d64695534c331967428c56
Administrador <diogarcia2@gmail.com>
2019-01-20 20:43:46 +0100

    I think it's done

--- app/src/main/java/com/example/administrador/updatetemps/MainActivity.java
+++ app/src/main/java/com/example/administrador/updatetemps/MainActivity.java
@@ -59,58 +59,11 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected Void doInBackground(Void... voids) {
 
-            final int mes= Integer.parseInt(new SimpleDateFormat("dd-MM-yyyy").format(new Date()).split("-")[1]);;
+            final int mes= Integer.parseInt(new SimpleDateFormat("dd-MM-yyyy").format(new Date()).split("-")[1]) -1;;
             final Double [] rains = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
             Map<String,Object> tempz = new HashMap<>();
-            try {
-                Document webPage;
 
-                webPage = Jsoup.connect("https://www.avamet.org/mxo_i.php?id=c04m139e01").get();
-                rains[0] = formatTemps(webPage.getElementById("prec").text());
-
-                webPage = Jsoup.connect("https://www.avamet.org/mxo_i.php?id=c04m055e02").get();
-                rains[1] = formatTemps(webPage.getElementById("prec").text());
-
-                webPage = Jsoup.connect("https://www.avamet.org/mxo_i.php?id=c04m001e02").get();
-                rains[2] = formatTemps(webPage.getElementById("prec").text());
-
-                webPage = Jsoup.connect("https://www.avamet.org/mxo_i.php?id=c02m129e02").get();
-                rains[3] = formatTemps(webPage.getElementById("prec").text());
-
-                webPage = Jsoup.connect("https://www.avamet.org/mxo_i.php?id=c99m044e15").get();
-                rains[4] = formatTemps(webPage.getElementById("prec").text());
-
-                webPage = Jsoup.connect("https://www.avamet.org/mxo_i.php?id=c08m131e01").get();
-                rains[5] = formatTemps(webPage.getElementById("prec").text());
-
-                webPage = Jsoup.connect("https://www.avamet.org/mxo_i.php?id=c06m084e02").get();
-                rains[6] = formatTemps(webPage.getElementById("prec").text());
-
-                webPage = Jsoup.connect("https://www.avamet.org/mxo_i.php?id=c99m044e01").get();
-                rains[7] = formatTemps(webPage.getElementById("prec").text());
-
-                webPage = Jsoup.connect("https://www.avamet.org/mxo_i.php?id=c07m115e01").get();
-                rains[8] = formatTemps(webPage.getElementById("prec").text());
-
-                webPage = Jsoup.connect("https://www.avamet.org/mxo_i.php?id=c09m201e01").get();
-                rains[9] = formatTemps(webPage.getElementById("prec").text());
-
-                webPage = Jsoup.connect("http://www.aemet.es/es/eltiempo/observacion/ultimosdatos?k=arn&l=8486X&w=1&datos=img").get();
-                rains[10] = formatTemps(webPage.getElementsByClass("fila_impar").get(4).text());
-
-                webPage = Jsoup.connect("http://www.aemet.es/es/eltiempo/observacion/ultimosdatos?k=val&l=8472A&w=1&datos=img&f=tmax").get();
-                rains[11] = formatTemps( webPage.getElementsByClass("fila_impar").get(4).text());
-
-                webPage = Jsoup.connect("https://meteosabi.es/el-tiempo-en-bronchales-teruel").get();
-                rains[12] = formatTemps(webPage.getElementById("RainToday").text());
-
-                //Log.d(TAG, "xyz_vect RAINS MODIFIIED: : "+Arrays.toString(rains));
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-
+            fillVectRains(rains);
 
             DocumentReference docRef = db.collection("Temperaturas").document("Estaciones");
             docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
@@ -119,40 +72,43 @@ public class MainActivity extends AppCompatActivity {
                     if (task.isSuccessful()) {
                         DocumentSnapshot document = task.getResult();
                         if (document.exists()) {
-                            Log.d(TAG, "xyz: BASE_DE_DATOS_ENCONTRADA data: " + document.getData());
+                            Map<String, Object> tempz = null;
 
-                            Map<String, Object> tempz = document.getData();
+                            tempz = document.getData();
+
                             ArrayList<Double> ad = null;
-                            //Log.d(TAG,"xyz_AQUI LLEGA");
 
-
-                            Log.d(TAG, "xyz_vect RAINS: " + Arrays.toString(rains));
-
-
-                            //MISTERIO DE LOS GORDOS
-                            //for (Double d : rains){
-                            //  Log.d(TAG,"xyz_R: "+d.toString());
-                            //}
+                            Log.d(TAG, "xyz_VECTOR LLUVIAS: " + Arrays.toString(rains));
 
                             for (int i = 0; i < rains.length; i++) {
+
                                 if(rains[i] > 0.0 ){
-                                    Log.d(TAG, "xyzRAIN: " + Integer.toString(i) + " - " + rains[i].toString());
+
+                                    Log.d(TAG, "xyz i position: "+Integer.toString(i) + " - Rain value: " + rains[i].toString());
+
                                     ad =(ArrayList<Double>) tempz.get("E"+Integer.toString(i));
-                                    Log.d(TAG,"xyz_QUE DISE #"+(ad.get(mes)+rains[i])+"#");
+
+                                    //Log.d(TAG, "xyzVECT STRING: "+ad.toString());
+
+                                    ad.set(mes,ad.get(mes)+rains[i]);
+
+                                    db.collection("Temperaturas").document("Estaciones")
+                                            .update(
+                                                    "E"+Integer.toString(i), ad
+                                            );
+                                    //ad =(ArrayList<Double>) tempz.get("E"+Integer.toString(i));
+                                    //Log.d(TAG,"xyzCLASS TYPE: "+ad.get(0).getClass().getName() );
                                     //double suma = ad.get(mes) ;
-                                    Log.d(TAG,"xyz_QUE DISE??");
+                                    //Log.d(TAG,"xyz_QUE DISE??");
                                     //ad.set(mes, suma);
-                                    Log.d(TAG,"xyz_ARRAY ACTUALISADO");
-                            }
+
+                                }
                                 /*
                                     Log.d(TAG,"xyzSUMA: "+Double.toString(suma));
                                     ad.set(mes, suma);
                                 */
                             }
-                            Log.d(TAG, "xyzSALE_DEL_FOR");
-
-                           // ad.set(mes, ad.get(mes) + rains[0] );
-
+                            Log.d(TAG,"xyz_SALE DEL FOR");
 
                         } else {
                             Log.d(TAG, "xyz No ENCUENTRA LA BBDD");
@@ -162,6 +118,10 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
             });
+
+
+            // ad.set(mes, ad.get(mes) + rains[0] );
+
 
 
             return null;
@@ -193,7 +153,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public Double formatTemps(String orgData){
+    public static Double formatTemps(String orgData){
         Double result;
         int idx = 0;
         for (int i=0;i<orgData.length();i++){
@@ -224,6 +184,56 @@ public class MainActivity extends AppCompatActivity {
             return 0.0;
         }
         return result;
+    }
+
+    public static void fillVectRains(Double [] rains){
+        try {
+            Document webPage;
+
+            webPage = Jsoup.connect("https://www.avamet.org/mxo_i.php?id=c04m139e01").get();
+            rains[0] = formatTemps(webPage.getElementById("prec").text());
+
+            webPage = Jsoup.connect("https://www.avamet.org/mxo_i.php?id=c04m055e02").get();
+            rains[1] = formatTemps(webPage.getElementById("prec").text());
+
+            webPage = Jsoup.connect("https://www.avamet.org/mxo_i.php?id=c04m001e02").get();
+            rains[2] = formatTemps(webPage.getElementById("prec").text());
+
+            webPage = Jsoup.connect("https://www.avamet.org/mxo_i.php?id=c02m129e02").get();
+            rains[3] = formatTemps(webPage.getElementById("prec").text());
+
+            webPage = Jsoup.connect("https://www.avamet.org/mxo_i.php?id=c99m044e15").get();
+            rains[4] = formatTemps(webPage.getElementById("prec").text());
+
+            webPage = Jsoup.connect("https://www.avamet.org/mxo_i.php?id=c08m131e01").get();
+            rains[5] = formatTemps(webPage.getElementById("prec").text());
+
+            webPage = Jsoup.connect("https://www.avamet.org/mxo_i.php?id=c06m084e02").get();
+            rains[6] = formatTemps(webPage.getElementById("prec").text());
+
+            webPage = Jsoup.connect("https://www.avamet.org/mxo_i.php?id=c99m044e01").get();
+            rains[7] = formatTemps(webPage.getElementById("prec").text());
+
+            webPage = Jsoup.connect("https://www.avamet.org/mxo_i.php?id=c07m115e01").get();
+            rains[8] = formatTemps(webPage.getElementById("prec").text());
+
+            webPage = Jsoup.connect("https://www.avamet.org/mxo_i.php?id=c09m201e01").get();
+            rains[9] = formatTemps(webPage.getElementById("prec").text());
+
+            webPage = Jsoup.connect("http://www.aemet.es/es/eltiempo/observacion/ultimosdatos?k=arn&l=8486X&w=1&datos=img").get();
+            rains[10] = formatTemps(webPage.getElementsByClass("fila_impar").get(4).text());
+
+            webPage = Jsoup.connect("http://www.aemet.es/es/eltiempo/observacion/ultimosdatos?k=val&l=8472A&w=1&datos=img&f=tmax").get();
+            rains[11] = formatTemps( webPage.getElementsByClass("fila_impar").get(4).text());
+
+            webPage = Jsoup.connect("https://meteosabi.es/el-tiempo-en-bronchales-teruel").get();
+            rains[12] = formatTemps(webPage.getElementById("RainToday").text());
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
 
